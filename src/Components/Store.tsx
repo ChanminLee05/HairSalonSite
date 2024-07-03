8bab2aa020de6a12f5d4361f8dae8b427b937017
Chanmin Lee <lee00837@algonquinlive.com>
2024-07-02 23:16:10 -0600

    modified page width

--- src/Components/Store.tsx
+++ src/Components/Store.tsx
@@ -10,7 +10,7 @@ interface OwnProps {
 const Store:React.FC<OwnProps> = ({info}) => {
     return (
         <>
-            <NavBar info={info}/>
+            <NavBar />
             <MainPage info={info}/>
         </>
     );
